7a990875ad49060a993aceecf01e3f4a048fde57
kennyrkun <keennymccormic+github@gmail.com>
2017-05-21 17:10:02 -0500

    Add project files.

---
+++ SFUI/Button.cpp
@@ -0,0 +1,12 @@
+#include "Button.hpp"
+
+#include <stdexcept>
+#include <iostream>
+
+namespace SFUI
+{
+	static double print(std::string str)
+	{
+		std::cout << str << std::endl;
+	}
+}
---
+++ SFUI/Button.hpp
@@ -0,0 +1,12 @@
+// Button.hpp
+
+#include <string>
+
+namespace SFUI
+{
+	class Button
+	{
+	public:
+		static double print(std::string str);
+	};
+}
