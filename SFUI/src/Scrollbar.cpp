9ec8952497e7bc8873a1d7de85e382c9967385b8
Kenny McCormick <keennymccormic+github@gmail.com>
2017-10-07 02:09:48 -0500

    updated scrollbar

--- SFUI/src/Scrollbar.cpp
+++ SFUI/src/Scrollbar.cpp
@@ -2,7 +2,6 @@
 
 #include <iostream>
 
-namespace SFUI {
 Scrollbar::Scrollbar()
 {
 	// do stuff
@@ -15,58 +14,139 @@ Scrollbar::~Scrollbar()
 
 void Scrollbar::create(sf::RenderWindow* target_window)
 {
-	std::cout << "creating scrollbar" << std::endl;
+	std::cout << "creating scrollbar" << "\n";
 
 	targetWindow = target_window;
 
 	scrollbar.setSize(sf::Vector2f(10, targetWindow->getSize().y));
-	scrollbar.setOrigin(sf::Vector2f(scrollbar.getLocalBounds().width / 2, scrollbar.getLocalBounds().height / 2));
-	scrollbar.setPosition((targetWindow->getDefaultView().getCenter().x * 2) - scrollbar.getLocalBounds().width / 2, targetWindow->getDefaultView().getCenter().y);
-	scrollbar.setFillColor(sf::Color(60, 60, 60));
+	scrollbar.setOrigin(sf::Vector2f(scrollbar.getLocalBounds().width / 2, 0));
+	scrollbar.setPosition((targetWindow->getView().getCenter().x * 2) - scrollbar.getLocalBounds().width / 2, targetWindow->getView().getCenter().y);
+	scrollbar.setFillColor(sf::Color(80, 80, 80));
 
-	scrollbarThumb.setSize(sf::Vector2f(scrollbar.getSize().x, targetWindow->getSize().y));
-	scrollbarThumb.setOrigin(sf::Vector2f(scrollbarThumb.getLocalBounds().width / 2, scrollbarThumb.getLocalBounds().height / 2));
-	scrollbarThumb.setPosition((targetWindow->getDefaultView().getCenter().x * 2) - scrollbarThumb.getLocalBounds().width / 2, targetWindow->getDefaultView().getCenter().y);
-	scrollbarThumb.setFillColor(sf::Color(100, 100, 100));
+	scrollThumb.setSize(sf::Vector2f(0, 0));
+	scrollThumb.setFillColor(sf::Color(110, 110, 110));
 
 	scrollJumpMultiplier = 16;
 }
 
 void Scrollbar::update(float contentHeight_, float viewportHeight)
 {
-	// a lot of this came from stack overflow, but i forgot exactly what question ;-;
+	std::cout << "updating scrollbar" << "\n";
 
-	contentHeight_ += 8; // padding at the end
+	scrollbar.setSize(sf::Vector2f(10, targetWindow->getSize().y));
+	scrollbar.setOrigin(sf::Vector2f(scrollbar.getLocalBounds().width / 2, 0));
+	scrollbar.setPosition(sf::Vector2f((targetWindow->getView().getCenter().x * 2) - scrollbar.getLocalBounds().width / 2, 0));
+
+	scrollThumb.setSize(sf::Vector2f(scrollbar.getSize().x, targetWindow->getSize().y));
+	scrollThumb.setOrigin(sf::Vector2f(scrollThumb.getLocalBounds().width / 2, 0));
+	scrollThumb.setPosition(sf::Vector2f((targetWindow->getView().getCenter().x * 2) - scrollThumb.getLocalBounds().width, targetWindow->getView().getCenter().y / 2));
+
 	contentHeight = contentHeight_;
-	maxHeight = contentHeight;
-	minHeight = 0;
+	min = scrollbar.getPosition().y;
+	max = contentHeight;
 
-	float viewableRatio = viewportHeight / contentHeight;
-	float thumbHeight = viewportHeight * viewableRatio;
+	float viewableRatio = viewportHeight / contentHeight; // 1/3 or 0.333333333n
+	float thumbHeight = viewportHeight * viewableRatio; // 50px
 
-	// calculate scroll step
+														// calculate scroll step
 	float scrollTrackSpace = contentHeight - viewportHeight;
 	float scrollThumbSpace = viewportHeight - thumbHeight;
 	scrollJump = (scrollTrackSpace / scrollThumbSpace) * scrollJumpMultiplier;
 
-	scrollbarThumb.setSize(sf::Vector2f(scrollbarThumb.getSize().x, thumbHeight));
-	scrollbarThumb.setOrigin(scrollbarThumb.getLocalBounds().width / 2, scrollbarThumb.getLocalBounds().height / 2);
-	scrollbarThumb.setPosition(scrollbar.getPosition().x, scrollbarThumb.getLocalBounds().height / 2);
+	scrollThumb.setSize(sf::Vector2f(scrollThumb.getSize().x, thumbHeight));
+	scrollThumb.setOrigin(sf::Vector2f(scrollThumb.getLocalBounds().width / 2, 0));
+	scrollThumb.setPosition(scrollbar.getPosition());
+
+	scrollThumbTopPosition = scrollThumb.getPosition().y;
+	scrollThumbBottomPosition = scrollThumb.getPosition().y + scrollThumb.getLocalBounds().height;
+	scrollbarTopPosition = scrollbar.getPosition().y;
+	scrollbarBottomPosition = scrollbar.getPosition().y + scrollbar.getLocalBounds().height;
+
+	if (scrollThumb.getSize().y >= scrollbar.getSize().y)
+	{
+		isEnabled = false;
+		scrollbar.setSize(sf::Vector2f(0, 0));
+		std::cout << "scrollbar disabled" << "\n";
+	}
+	else
+	{
+		isEnabled = true;
+		std::cout << "scrollbar enabled" << "\n";
+	}
 }
 
-void Scrollbar::moveThumbUp()
+bool Scrollbar::canScrollDown()
 {
-	scrollbarThumb.move(0, 1 * scrollJumpMultiplier);
+	return (scrollThumbBottomPosition < scrollbarBottomPosition);
 }
 
 void Scrollbar::moveThumbDown()
 {
-	scrollbarThumb.move(0, -1 * scrollJumpMultiplier);
+	if (scrollThumbBottomPosition < scrollbarBottomPosition)
+	{
+		scrollThumb.move(0, 1 * scrollJumpMultiplier);
+
+		updateLimits();
+
+		if (scrollThumbBottomPosition > scrollbarBottomPosition) // clamp
+		{
+			std::cout << "scrollbar went too far down (" << scrollThumbBottomPosition - scrollbarBottomPosition << "), clamping..." << "\n";
+			moveToBottom();
+			updateLimits();
+		}
+	}
+	else
+	{
+		std::cout << "cannot scroll down (" << scrollThumbBottomPosition << " < " << scrollbarBottomPosition << ")" << "\n";
+	}
 }
 
-void Scrollbar::draw()
+bool Scrollbar::canScrollUp()
 {
-	targetWindow->draw(scrollbar);
-	targetWindow->draw(scrollbarThumb);
+	return (scrollThumbTopPosition > scrollbarTopPosition);
 }
-} // namespace
+
+void Scrollbar::moveThumbUp()
+{
+	if (scrollThumbTopPosition > scrollbarTopPosition)
+	{
+		scrollThumb.move(0, -1 * scrollJumpMultiplier);
+
+		updateLimits();
+
+		if (scrollThumbTopPosition < scrollbarTopPosition) // clamp
+		{
+			std::cout << "scrollbar went too far up (" << scrollbarTopPosition - scrollThumbTopPosition << "), clamping..." << "\n";
+			moveToTop();
+			updateLimits();
+		}
+	}
+	else
+	{
+		std::cout << "cannot scroll up (" << scrollThumbTopPosition << " < " << scrollbarTopPosition << ")" << "\n";
+	}
+}
+
+void Scrollbar::moveToTop()
+{
+	scrollThumb.setPosition(scrollThumb.getPosition().x, scrollbarTopPosition);
+}
+
+void Scrollbar::moveToBottom()
+{
+	scrollThumb.setPosition(scrollThumb.getPosition().x, scrollbarBottomPosition - scrollThumb.getLocalBounds().height);
+}
+
+void Scrollbar::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+	target.draw(scrollbar);
+	target.draw(scrollThumb);
+}
+
+void Scrollbar::updateLimits()
+{
+	scrollThumbTopPosition = scrollThumb.getPosition().y;
+	scrollThumbBottomPosition = scrollThumb.getPosition().y + scrollThumb.getLocalBounds().height;
+	scrollbarTopPosition = scrollbar.getPosition().y;
+	scrollbarBottomPosition = scrollbar.getPosition().y + scrollbar.getLocalBounds().height;
+}
--- SFUI/src/Scrollbar.hpp
+++ SFUI/src/Scrollbar.hpp
@@ -3,31 +3,45 @@
 
 #include <SFML\Graphics.hpp>
 
-namespace SFUI
+class Scrollbar : public sf::Drawable
 {
-	class Scrollbar
-	{
-	public:
-		Scrollbar();
-		~Scrollbar();
+public:
+	Scrollbar();
+	~Scrollbar();
 
-		void create(sf::RenderWindow* target_window);
-		void update(float contentHeight, float viewportHeight);
-		void moveThumbUp();
-		void moveThumbDown();
-		void draw();
+	void create(sf::RenderWindow* target_window);
+	void update(float contentHeight_, float viewportHeight);
 
-		sf::RectangleShape scrollbar;
-		sf::RectangleShape scrollbarThumb;
-		float scrollJump = 0;
-		float scrollJumpMultiplier = 16;
-		float maxHeight;
-		float minHeight;
-		float contentHeight;
+	bool isEnabled;
 
-	private:
-		sf::RenderWindow* targetWindow;
-	};
-}
+	bool canScrollDown();
+	void moveThumbDown();
+	bool canScrollUp();
+	void moveThumbUp();
+	void moveToTop();
+	void moveToBottom();
+
+	void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+
+	sf::RectangleShape scrollbar;
+	sf::RectangleShape scrollThumb;
+
+	bool thumbDragging;
+	float min; // minimum scrollbar position, in pixels
+	float max; // maximum scrollbar position, in pixels
+	float scrollJump = 0; // amount to move the thing that moves, in pixels
+	float scrollJumpMultiplier = 16; // amount to move the thumb, in pixels
+	float contentHeight; // height of the content, in pixels
+
+	float scrollbarTopPosition;
+	float scrollbarBottomPosition;
+	float scrollThumbTopPosition;
+	float scrollThumbBottomPosition;
+
+private:
+	sf::RenderWindow* targetWindow;
+
+	void updateLimits();
+};
 
 #endif
